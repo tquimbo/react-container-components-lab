148f4620e94e4179ee305572b025897da9b4e73c
Tim Quimbo <tquimbo1@gmail.com>
2021-12-06 00:01:17 -0500

    commit tree b718ceac7cc376db96b81bd1d8374542dcc818c0

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,9 @@
-// Code MovieReviews Here
+import React from 'react';
+ 
+const MovieReviews = ({ reviews }) => (
+  <div className="review-list">
+    { reviews.map(review => <div className="review">{review.display_title}</div>) }
+  </div>
+)
+ 
+export default MovieReviews;
